411e254320f369147af3b9d29febcc02fe8b1486
pcastiglione99 <pcastiglione99@gmail.com>
2025-04-14 12:37:41 +0200

    initial commit

---
+++ voli/src/main/java/com/voli/voli/VoliApplication.java
@@ -0,0 +1,13 @@
+package com.voli.voli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoliApplication.class, args);
+	}
+
+}
---
+++ voli/src/test/java/com/voli/voli/VoliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voli.voli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
